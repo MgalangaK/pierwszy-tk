5d30a6af63016199f1911a71b371e426ec8baf60
mateusz <mateusz.krystkiewicz@cyfroweszkoly.pl>
2022-09-29 13:54:05 +0200

    applay grid change pack to grid

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ root.minsize(250,250)
 
 lb1=Label()
 lb1["text"] = "label 1 :)"
-lb1.pack()
+lb1.grid(column=0, row=1)
 
 
 lb2=Label()
 lb2.config(text="label 2 :)")
-lb2.pack()
+lb2.grid(column=0, row=2)
 
 def change_label2_on_click():
     lb2.config(text=entry.get())
 
 btn = Button()
 btn.config(text="przycisk", command=change_label2_on_click)
-btn.pack()
+btn.grid(column=0, row=3)
 
 entry = Entry()
-entry.pack()
+entry.grid(column=0, row=4)
 
 
 
