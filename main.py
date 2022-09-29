cfa094d861fe693ceff2e8656554e40019ddbed6
mateusz <mateusz.krystkiewicz@cyfroweszkoly.pl>
2022-09-29 13:48:19 +0200

    add labels add first labels

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from tk import tk
+from tkinter import Tk,Label
 
 root = Tk()
 root.minsize(250,250)
+
+lb1=Label()
+lb1["text"] = "label 1 :)"
+lb1.pack()
+
+
+lb2=Label()
+lb2.config(text="label 2 :)")
+lb2.pack()
+
 root.mainloop()
